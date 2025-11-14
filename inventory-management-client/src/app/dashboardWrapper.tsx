4c836ec4ef4dd13524a654845662f2b08b6b71e2
luke123adams <lxa207@gmail.com>
2025-11-14 15:13:27 +0000

    installed redux and added reducers

--- inventory-management-client/src/app/dashboardWrapper.tsx
+++ inventory-management-client/src/app/dashboardWrapper.tsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import Navbar from "@/app/(components)/Navbar"
+import Sidebar from './(components)/Sidebar';
+import StoreProvider from './redux';
 
-const DashboardWrapper = ({ children } : { children: React.ReactNode }) => {
+const DashboardLayout = ({ children } : { children: React.ReactNode }) => {
   return (
     <div className={`light flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
-        SideBar
+        <Sidebar />
         <main className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-24`}>
             <Navbar />
             {children}
             </main>
         </div>
+  );
+};
+
+const DashboardWrapper = ({ children } : { children: React.ReactNode }) => {
+  return (
+    <StoreProvider>
+      <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
   );
 };
 
---
+++ inventory-management-client/src/state/index.ts
@@ -0,0 +1,28 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+export interface InitialStateTypes {
+    isSideBarCollapsed: boolean;
+    isDarkMode: boolean;
+}
+
+const initialState: InitialStateTypes = {
+    isSideBarCollapsed: false,
+    isDarkMode: false,
+}
+
+export const globalSlice = createSlice({
+    name: 'global',
+    initialState,
+    reducers: {
+        setIsSideBarCollapsed: (state, action: PayloadAction<boolean>) => {
+            state.isSideBarCollapsed = action.payload;
+        },
+        setIsDarkMode: (state, action: PayloadAction<boolean>) => {
+            state.isDarkMode = action.payload;
+        },
+    },
+});
+
+export const { setIsSideBarCollapsed, setIsDarkMode } = globalSlice.actions;
+
+export default globalSlice.reducer
